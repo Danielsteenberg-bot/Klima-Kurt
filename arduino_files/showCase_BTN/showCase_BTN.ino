224f7ced41fe30c1b6a233660e20b058b32510e9
cbc1000 <127729826+cbc1000@users.noreply.github.com>
2023-11-01 11:05:51 +0100

    swaggermis

--- arduino_files/showCase_BTN/showCase_BTN.ino
+++ arduino_files/showCase_BTN/showCase_BTN.ino
@@ -7,7 +7,7 @@ MKRIoTCarrier carrier;
 uint32_t colorRed = carrier.leds.Color(200, 0, 0);
 uint32_t colorGreen = carrier.leds.Color(0, 200, 0);
 uint32_t colorBlue = carrier.leds.Color(0, 0, 200);
-
+bool codeEntered = false;
 
 int noteDurations[] = {
   4, 4, 4, 4, 4, 2, 8, 8, 8, 8, 8, 4, 4, 4, 4, 4, 2, 8, 8, 8, 8, 8, 4, 4, 4, 4, 4,
@@ -30,7 +30,7 @@ touchButtons buttons[] = {TOUCH0, TOUCH1, TOUCH2, TOUCH3, TOUCH4};
 char buttonNR[] = "01234";
 int xCoordinate = 85;  
 
-void setup() {
+void startDisplay() {
   carrier.begin();
   CARRIER_CASE = true;
   Serial.begin(9600);
@@ -43,10 +43,31 @@ void setup() {
   carrier.display.setCursor(50, 90); 
   carrier.display.print("kode"); 
 
+  count = 0;
+  xCoordinate = 85;
+  for (int i = 0; i < 5; i++) {
+    carrier.leds.setPixelColor(i, 0);
+ }
+
+ Serial.println("Display started.");
+}
+
+void setup() {
+  startDisplay();
 }
 
 void loop() {
-  btnRegister();
+  if (!codeEntered) {
+    btnRegister();}
+  else {
+    Serial.println("Waiting for 5 seconds...");
+    delay(5000);
+    codeEntered = false;
+    Serial.println("Starting display again...");
+    delay(500);
+    startDisplay();
+    
+  }
 }
 
 void btnRegister() {
@@ -64,7 +85,7 @@ void btnRegister() {
 
       if (count == 5) {
         delay(1000);
-        carrier.leds.setPixelColor(i, colorGreen);
+        carrier.leds.fill(colorGreen, 0, 5);
         carrier.leds.show();
         playMelody();
         carrier.display.fillScreen(0x0000);
@@ -75,6 +96,7 @@ void btnRegister() {
         delay(5000);
         carrier.leds.clear();
         carrier.leds.show();
+        codeEntered = true;
       }
     }
   }
