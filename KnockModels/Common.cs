e33f2ef01ed06ee0359244669493f6ca400cc2e8
Your Name <you@example.com>
2023-11-09 09:42:17 +0100

    scam

--- KnockModels/Common.cs
+++ KnockModels/Common.cs
@@ -2,12 +2,22 @@
 {
     public class Common
     {
+
+
         public int Id { get; set; }
 
         public DateTime CreatedAt { get; set; }
 
         public DateTime? UpdatedAt { get; set; }
+
+        public Common()
+        {
+            this.CreatedAt = DateTime.Now;
+            this.UpdatedAt = DateTime.Now;
+        }
+
     }
+
 
 
 }
