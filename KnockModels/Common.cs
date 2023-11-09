d4d415f834d4abb22af1cb3d3cc9a7508061f216
Your Name <you@example.com>
2023-11-09 07:56:33 +0100

    User Email added

--- KnockModels/Common.cs
+++ KnockModels/Common.cs
@@ -8,4 +8,6 @@
 
         public DateTime? UpdatedAt { get; set; }
     }
+
+
 }
--- KnockModels/User.cs
+++ KnockModels/User.cs
@@ -10,6 +10,8 @@ namespace KnockModels
     {
         public string Name { get; set; }
 
+        public string Email { get; set; }
+
         public Code Code { get; set; }
     }
 }
